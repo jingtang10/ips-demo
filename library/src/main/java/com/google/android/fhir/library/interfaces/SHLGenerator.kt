421ceb6518a1bcfcb1d48558f70786fcc46281c8
Hugo Miloszewski <hugomilosz@google.com>
2023-09-18 14:02:09 +0000

    Changes to SHLGen interface

--- library/src/main/java/com/google/android/fhir/library/interfaces/SHLGenerator.kt
+++ library/src/main/java/com/google/android/fhir/library/interfaces/SHLGenerator.kt
@@ -1,8 +1,12 @@
 package com.google.android.fhir.library.interfaces
 
+import android.content.Context
 import com.google.android.fhir.library.dataClasses.SHLData
 
 interface SHLGenerator {
+  // Should generateKey be part of this interface or should it just be in utils?
   fun generateKey() : String
-  fun generateSHL(data : SHLData) : SHLData
+
+  // Passcode added to generateSHL as it shouldn't be wrapped in the SHL object
+  fun generateSHL(context: Context, data : SHLData, passcode: String) : SHLData
 }
