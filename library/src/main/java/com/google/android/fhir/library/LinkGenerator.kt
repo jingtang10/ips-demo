2d8c07e65d8fdbb825ce6ee1d91f43084c5c93f6
Hugo Miloszewski <hugomilosz@google.com>
2023-09-20 15:05:37 +0000

    Implemented linkGenerator

--- library/src/main/java/com/google/android/fhir/library/LinkGenerator.kt
+++ library/src/main/java/com/google/android/fhir/library/LinkGenerator.kt
@@ -1,14 +1,19 @@
 package com.google.android.fhir.library
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.google.android.fhir.library.dataClasses.SHLData
 import com.google.android.fhir.library.interfaces.SHLGenerator
+import com.google.android.fhir.library.utils.GenerateShlUtils
 import java.security.SecureRandom
 import java.util.Base64
 
 class LinkGenerator : SHLGenerator {
+
+  val generateShlUtils = GenerateShlUtils()
+
   @RequiresApi(Build.VERSION_CODES.O)
   override fun generateKey(): String {
     val random = SecureRandom()
@@ -17,8 +22,13 @@ class LinkGenerator : SHLGenerator {
     return Base64.getUrlEncoder().encodeToString(keyBytes)
   }
 
-  override fun generateSHL(context: Context, data: SHLData, passcode: String): SHLData {
-    TODO("Not yet implemented")
-  }
+  @RequiresApi(Build.VERSION_CODES.O)
+  override fun generateSHL(context: Context, shlData: SHLData, passcode: String): Bitmap? {
+    var bitmap: Bitmap? = null
+    generateShlUtils.generateAndPostPayload(passcode, shlData, context) { output ->
+      bitmap = output
+    }
 
+    return bitmap
+  }
 }
